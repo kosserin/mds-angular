360f32233f37e8e2b629cca9d28ec66718916d81
Андрија <kostic.andrija00@gmail.com>
2025-03-24 13:02:59 +0100

    Add border color based on status for task items

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,12 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Task } from 'src/app/models/task.model';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Status } from "src/app/models/status.model";
+import { Task } from "src/app/models/task.model";
 
 @Component({
-  selector: 'mds-task-item',
-  templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.scss']
+  selector: "mds-task-item",
+  templateUrl: "./task-item.component.html",
+  styleUrls: ["./task-item.component.scss"],
 })
 export class TaskItemComponent {
   @Input() task!: Task;
+  @Input() status!: Status;
   @Output() open = new EventEmitter<Task>();
+
+  getTaskClass(status: Status): string {
+    switch (status.color) {
+      default:
+      case "grey":
+        return "grey";
+      case "blue":
+        return "blue";
+      case "green":
+        return "green";
+    }
+  }
 }
